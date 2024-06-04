e761c4bc61bc056d21c71a1eda998efeea5ecca4
MP3095 <mariapauladelgadogonzalez3@gmail.com>
2024-06-04 13:45:25 -0500

    3

--- 3/vue.config.js
+++ 3/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .test(/\.js$/)
+      .exclude
+      .add(filepath => {
+        // Aquí puedes agregar cualquier exclusión adicional si es necesario
+        return /node_modules\/(?!chart.js)/.test(filepath);
+      })
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader');
+  }
+};
